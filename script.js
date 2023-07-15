359a3175e568447fedd2dfa8cd2e889289c840ae
Ajung007 <agungrachmadprihanto@gmail.com>
2023-07-15 10:53:45 +0700

    project selesai

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ $(document).ready(function () {
     } else {
       $(".navbar").removeClass("sticky");
     }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
   });
 
   // toggle menu/navarbar
@@ -13,6 +23,13 @@ $(document).ready(function () {
     $(".navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
   });
+});
+
+var typed = new Typed(".typing", {
+  strings: ["Youtuber", "Developer", "Blogger", "Sleeper"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
 });
 
 // owl carousel script
